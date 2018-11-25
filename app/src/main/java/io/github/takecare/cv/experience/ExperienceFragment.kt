f5ceb1b4cabe7b7fd6f15b1d1d0673671cf4c4b1
takecare <rui@vazteixeira.org>
2018-11-25 21:45:34 +0000

    display data

--- app/src/main/java/io/github/takecare/cv/experience/ExperienceFragment.kt
+++ app/src/main/java/io/github/takecare/cv/experience/ExperienceFragment.kt
@@ -4,11 +4,13 @@ import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.github.takecare.cv.R
 import kotlinx.android.synthetic.main.fragment_experience.*
+import kotlinx.android.synthetic.main.item_experience.view.*
 import javax.inject.Inject
 
 class ExperienceFragment : Fragment(), ExperienceView {
@@ -19,6 +21,7 @@ class ExperienceFragment : Fragment(), ExperienceView {
 
     @Inject
     lateinit var presenter: ExperiencePresenter
+    private val adapter = ExperienceAdapter()
 
     override fun onAttach(context: Context?) {
         super.onAttach(context)
@@ -38,7 +41,7 @@ class ExperienceFragment : Fragment(), ExperienceView {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         recyclerview.layoutManager = LinearLayoutManager(activity)
-//        recyclerview.adapter =
+        recyclerview.adapter = adapter
     }
 
     override fun onResume() {
@@ -52,10 +55,43 @@ class ExperienceFragment : Fragment(), ExperienceView {
     }
 
     override fun show(experienceViewModel: ExperienceViewModel) {
-        // TODO: show not implemented
+        adapter.update(experienceViewModel.items)
     }
 
     override fun showError(throwable: Throwable) {
         // TODO: showError not implemented
+    }
+}
+
+class ExperienceAdapter : RecyclerView.Adapter<ExperienceViewHolder>() {
+
+    private val data: MutableList<ExperienceItemViewModel> = mutableListOf()
+
+    override fun onCreateViewHolder(parent: ViewGroup, type: Int): ExperienceViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.item_cover, parent, false)
+        return ExperienceViewHolder(view)
+    }
+
+    override fun getItemViewType(position: Int) = 0
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(viewHolder: ExperienceViewHolder, position: Int) {
+        viewHolder.bind(data[position])
+    }
+
+    fun update(items: List<ExperienceItemViewModel>) {
+        data.clear()
+        data.addAll(items)
+        notifyDataSetChanged()
+    }
+}
+
+class ExperienceViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    fun bind(item: ExperienceItemViewModel) {
+        itemView.textview.text = item.name
     }
 }
